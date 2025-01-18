018da3600dbd4c2d357493f8f5e53402f4a68098
rabbir117991 <rabbitbestcute@gmail.com>
2025-01-19 06:51:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-from flask import Flask, render_template, jsonify 
+from flask import Flask, render_template, request, jsonify 
+import os
+from langchain_openai import OpenAIEmbeddings 
+from langchain_community.vectorstores import Chroma 
+from langchain.chains.question_answering import load_qa_chain 
+from langchain_community.callbacks import get_openai_callback 
+from langchain_openai import ChatOpenAI 
+from opencc import OpenCC
+import openai
+from openai import OpenAI
+client = OpenAI ( )
+openai.api_key = os. getenv('OPENAI_API_KEY')
 
 app = Flask(__name__)
 @app.route('/')
@@ -10,3 +21,24 @@ def hello():
 
 if __name__ == '__main__':
   app.run(debug=True)
+
+def get_response):
+user_input = request.form.get(user_input')
+db = None
+if not user_input:
+return jsonify({'error': 'No user input provided'})
+if user_input:
+embeddings = OpenAlEmbeddings)
+db = Chroma(persist_directory="./db/temp/",
+", embedding_function=embeddings)
+docs = b.similarity_search(user_input)
+Ilm = ChatOpenAl(
+model_name="gpt-4o"
+temperature=0.5
+chain = load_qa_chain(lIm, chain_type="stuff")
+with get_openai_callback() as cb:
+response = chain.invoke({"input_documents": docs, "question":user_input}, return_only_outputs=True)
+cc = OpenCC('s2t')
+answer=cc.convert(response|output_text'])
+chat_history.append(f'user': user_input, 'assistant': response['output_text']})
+return jsonify({'response': answer})
